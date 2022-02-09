3d2b823d577be3e40a58ee632e6235f99d717a1a
andeleabraham <58780376+andeleabraham@users.noreply.github.com>
2022-02-09 05:12:13 +0300

    The New Broadway gross sales for Thearter

--- grosses.R
+++ grosses.R
@@ -1,13 +1,16 @@
 
 ## read data;
 library(tidyverse)
-grosses <- read.csv("grosses.csv")
+grosses <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-28/grosses.csv")
 
 ##create month category and year category
-mnths <- c("JAN","FEB","MAR","APR","MAY","JUNE","JULY","AUG","SEPT","OCT","NOV","DEC")
 new_grosses <- grosses %>%
   mutate(year = as.numeric(str_sub(week_ending,1,4))) %>%
   mutate(month = as.numeric(str_sub(week_ending,6,7))) %>%
   mutate(preview_status = ifelse(previews>0,"Previewed", "No Previews")) %>%
-  mutate(month_name = mnths[month]) %>%
+  mutate(month_name = month.name[month]) %>%
   select(year, month,month_name, avg_ticket_price, seats_sold, preview_status)
+
+
+
+write.csv(new_grosses,"newGrosses.csv", row.names = FALSE)
